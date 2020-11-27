1c5983016e7b55c72e3015e3318a72ce00959aa8
Andrew5205 <andrewchung11@gmail.com>
2020-11-27 14:43:19 -0800

    add 15_string.py

---
+++ sec3/15_string.py
@@ -0,0 +1,16 @@
+
+# # string is ordered sequences ( have specific space for each single char)
+# # means can use indexing & slicing
+
+# char :          h   e   l   l   o
+# index:          0   1   2   3   4
+# reverse index:  0   -4  -3  -2  -1
+
+
+# slice string 
+# slice[start:stop:step]
+
+
+print("hello \n world")
+                            # hello 
+                            #  world
