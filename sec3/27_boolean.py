5a8691bd9ab7d6301523fcc7f32c59be0cc22d23
Andrew5205 <andrewchung11@gmail.com>
2020-11-28 15:20:27 -0800

    add 27_boolean.py

---
+++ sec3/27_boolean.py
@@ -0,0 +1,15 @@
+
+# True or False
+# must cap
+
+# None
+
+print(1 == 1)       # True
+# print(1 === 1)      # SyntaxError: invalid syntax
+
+print(1 > 2)        # False
+
+
+# let you claim a variable w/o assign a value 
+b = None
+
