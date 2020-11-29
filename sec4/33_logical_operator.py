e49e5870ba0b10b1a5f64e6aaa82b2854b2789b6
Andrew5205 <andrewchung11@gmail.com>
2020-11-29 09:42:21 -0800

    add 33_logical_operator.py

---
+++ sec4/33_logical_operator.py
@@ -0,0 +1,21 @@
+
+# and => &
+# or => |
+# not => !
+
+
+print((1 < 2) and (2 < 3))      # True
+print((1 < 2) & (2 < 3))        # True
+
+
+print((1 < 2) or ("a" == "b"))      # True
+print((1 < 2) | ("a" < "b"))        # True
+
+print("a" < "b")                    # True      # ASCII 97 < 98
+print("a" > "b")                    # False     # ASCII
+
+
+print(not True)     # False
+print(20 != 30)     # True
+print( 0 == 0)      # True  # differ then JS
+
