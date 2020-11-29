52c8049d999b6ce9e1ce406998710a3259fa01d1
Andrew5205 <andrewchung11@gmail.com>
2020-11-29 09:55:13 -0800

    add 34_if_elif_else.py

---
+++ sec5/34_if_elif_else.py
@@ -0,0 +1,32 @@
+
+
+# control flow
+
+# if 
+# elif
+# else
+
+# *************************
+# pattern 
+
+# if condition:
+#     do sth 
+# elif other condition:
+#     do sth diff
+# else:
+#     do the rest
+# *************************
+
+
+loc = "Bank"
+
+if loc == "Auto shop":
+    print("cars are cool")
+elif loc == "Bank":
+    print("money rock")
+else:
+    print("Where am I ?")
+
+# money rock
+
+
