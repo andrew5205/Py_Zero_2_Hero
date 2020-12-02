091d696a2ea0fa899d93682ce95e8b65e668109e
Andrew5205 <andrewchung11@gmail.com>
2020-12-01 21:36:54 -0800

    add 78_module_packages.py

---
+++ sec9/78_module_packages.py
@@ -0,0 +1,15 @@
+
+# module - same as another .py script
+
+# packages - collection of modules
+    # to let python know the file/ folder belong to a package 
+    # create __init__.py in same directory
+
+# from (package) import (module)
+
+
+
+
+
+
+
