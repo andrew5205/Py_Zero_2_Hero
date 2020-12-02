240ab9af267f49324c41de978ad2544e191ce807
Andrew5205 <andrewchung11@gmail.com>
2020-12-01 21:15:24 -0800

    add 79_name_main.py

---
+++ sec9/79_name_main.py
@@ -0,0 +1,25 @@
+
+
+# if __name__ == "__main__":
+
+# __name__ is a builtin variable
+
+# once enter "python file.py" in command line 
+
+# python will assign 
+# __name__ = "__main__"
+
+
+# so can look below line as "as soon as the script was executed "
+# if __name__ == "__main__":
+    # run the script
+
+# **************************************************************************
+
+
+print(__name__)         # __main__
+
+
+
+
+
