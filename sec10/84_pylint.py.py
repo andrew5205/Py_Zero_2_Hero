bd208fdef727279ba0d2f6f9d121ab995960c8af
Andrew5205 <andrewchung11@gmail.com>
2020-12-02 19:56:42 -0800

    add 84_pylint.py

---
+++ sec10/84_pylint.py.py
@@ -0,0 +1,31 @@
+
+# pylint - 
+
+# unittest - built-in libary
+
+
+
+a = 1
+b = 3
+
+print(a)
+print(B)
+
+# base) andrews-mbp-4:sec10 andrew$ pylint 84_pylint.py 
+# ************* Module 84_pylint
+# 84_pylint.py:21:0: C0305: Trailing newlines (trailing-newlines)
+# 84_pylint.py:1:0: C0103: Module name "84_pylint" doesn't conform to snake_case naming style (invalid-name)
+# 84_pylint.py:1:0: C0114: Missing module docstring (missing-module-docstring)
+# 84_pylint.py:8:0: C0103: Constant name "a" doesn't conform to UPPER_CASE naming style (invalid-name)
+# 84_pylint.py:9:0: C0103: Constant name "b" doesn't conform to UPPER_CASE naming style (invalid-name)
+# 84_pylint.py:12:6: E0602: Undefined variable 'B' (undefined-variable)
+
+# -------------------------------------
+# Your code has been rated at -15.00/10
+
+
+
+
+
+
+
