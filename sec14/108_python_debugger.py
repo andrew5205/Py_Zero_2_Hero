7f9679568d6e6b8b160f45225f727872a1237ab8
Andrew5205 <andrewchung11@gmail.com>
2020-12-12 14:45:15 -0800

    add 108_python_debugger.py

---
+++ sec14/108_python_debugger.py
@@ -0,0 +1,20 @@
+
+import pdb
+
+x = [1, 2, 3]
+y = 2
+z = 3 
+
+result_one = y + z 
+
+
+# pdb.set_trace() - allowed to pause and track variables 
+pdb.set_trace()
+
+result_two = x + y 
+
+
+
+
+
+
