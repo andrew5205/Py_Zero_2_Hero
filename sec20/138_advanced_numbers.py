a1aa278868448fc5abb279a517adf6ac9ca6aa21
Andrew5205 <andrewchung11@gmail.com>
2020-12-20 14:38:53 -0800

    add 138_advanced_numbers.py

---
+++ sec20/138_advanced_numbers.py
@@ -0,0 +1,33 @@
+# built-in
+# hex()
+# oct()
+# bin()
+# pow()
+# abs()
+#round()
+
+
+print(hex(512))         # 0x200
+
+print(oct(512))         # 0o1000
+
+print(bin(1024))        # 0b10000000000
+
+
+print(pow(2, 3))        # 8
+# pow(x ** y [% z])
+print(pow(2, 4, 3))     # 1
+
+
+print(abs(-2))          # 2
+
+print(round(3.9))       # 4
+print(round(3.1415926)) # 3
+
+
+
+
+
+
+
+
