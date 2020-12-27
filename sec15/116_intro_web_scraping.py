344fe07c62712b76f60a3b90fe74d2bcf3244436
Andrew5205 <andrewchung11@gmail.com>
2020-12-27 12:09:31 -0800

    add 116_intro_web_scraping.py

---
+++ sec15/116_intro_web_scraping.py
@@ -0,0 +1,34 @@
+
+# Rules of web scraping 
+# 1. Always try to get permission before scraping 
+# 2. IP may be locked if too many scraping attempt
+# 3. Some site auto block scraping software
+
+
+
+
+# Limitation of web scraping 
+# 1. in general every website is unique, means every web scraping script is unique
+# 2. a slight change or update to a web page may completely break web scraping script 
+
+
+
+# pip install requests - request to a web and get info 
+# pip install lxml 
+# pip install bs4
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
